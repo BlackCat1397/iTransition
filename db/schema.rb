a8b0bee1002a638a78b91710deaaea5c9d8b65d0
Roman Pavlov <romlho13@gmail.com>
2016-03-18 02:38:31 +0300

    Deleted all models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317299995) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "posts", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.string   "name",       limit: 255
-    t.decimal  "rating",                 precision: 10
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-  end
-
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.string   "password_digest", limit: 255
-    t.text     "photo",           limit: 65535
-    t.boolean  "admin"
-    t.string   "medals",          limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-  end
-
-  add_foreign_key "posts", "users"
 end
